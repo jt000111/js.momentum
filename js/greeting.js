9f7d7d95868ec6669d69a32c286b33e50a38cf1b
jt000111 <jt000111@naver.com>
2021-07-22 16:20:04 +0900

    clock

---
+++ js/greeting.js
@@ -0,0 +1,28 @@
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function paintGreetings(usernmaes) {
+  greeting.innerText = `Hello ${usernmaes}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
---
+++ js/quotes.js
@@ -0,0 +1,22 @@
+const quotes = [
+    {
+        quote: "I never dreamed about success, I worked for it."
+        author: "Estee Lauder"
+    },
+    {
+        quote: "Do not try to be original, just try to be good."
+        author: "Paul Rand"
+    },
+    {
+        quote: "Do not be afraid to give up the good to go for the great."
+        author: "John D.Rockefeller"
+    },
+    {
+        quote: "If you cannot fly then run. If you cannot run, then walk. And if you cannot walk, then crawl, but whatever you do, you have to keep moving forward."
+        author: "Martin Luther King Jr."
+    },
+    {
+        quote
+    }
+
+]
