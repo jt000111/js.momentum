26a596551e8262d301e0c274d4d92dc8cfbf1fd3
jt000111 <jt000111@naver.com>
2021-07-26 20:38:57 +0900

    ing

--- js/greeting.js
+++ js/greeting.js
@@ -1,14 +1,9 @@
-const loginForm = document.getElementById("login-form");
+const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
-
-function paintGreetings(usernmaes) {
-  greeting.innerText = `Hello ${usernmaes}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
-}
 
 function onLoginSubmit(event) {
   event.preventDefault();
@@ -16,6 +11,11 @@ function onLoginSubmit(event) {
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
+}
+
+function paintGreetings(usernames) {
+  greeting.innerText = `Hello ${userames}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
